cc4c685baef92c3ceed006a9dbd2370e63e8bafd
The Voice <57120898+momardia54@users.noreply.github.com>
2024-12-03 00:59:55 -0500

    Update hooks.py

--- tiered_pricing_moq/hooks.py
+++ tiered_pricing_moq/hooks.py
@@ -42,19 +42,19 @@ doc_events = {
 # tiered pricing calcutions for all sales processes
 doc_events = {
     "Quotation": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing_logic"
+        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing"
     },
     "Sales Order": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing_logic"
+        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing"
     },
     "Sales Invoice": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing_logic"
+        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing"
     },
     "Delivery Note": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing_logic"
+        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing"
     },
     "Sales Receipt": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing_logic"
+        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing"
     }
 }
 
