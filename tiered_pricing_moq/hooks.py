304e1eade6f44cb99f4e8a7b189a3ef1b9b17962
The Voice <57120898+momardia54@users.noreply.github.com>
2024-12-04 07:15:50 -0500

    Update hooks.py

--- tiered_pricing_moq/hooks.py
+++ tiered_pricing_moq/hooks.py
@@ -1,63 +1,49 @@
 app_name = "tiered_pricing_moq"
-app_title = "Tiered pricing moq"
-app_publisher = "Momar Dia	"
-app_description = "Apply tiered pricing and mo q"
+app_title = "Tiered Pricing MOQ"
+app_publisher = "Momar Dia"
+app_description = "Apply tiered pricing and MOQ"
 app_email = "momar@boostwaydigital.com"
-app_license = "agpl-3.0"
+app_license = "AGPL-3.0"
 
-# Apps
-# ------------------
+# Hooks for after installation
+# ----------------------------
+after_install = "tiered_pricing_moq.setup.setup_custom_fields"
 
-# create the required fields in the database and add them to the form
-after_install = "tiered_pricing_moq.setup.setup.setup_custom_fields"
-
-
-# MQO validation hooks ______________________________________________________________________________________________________________
+# Document Events
+# ----------------------------
 doc_events = {
+    # MOQ validation hooks and tiered pricing calculation
     "Quotation": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.moq_validation_script.validate_moq"
+        "before_save": [
+            "tiered_pricing_moq.moq_validation_script.validate_moq",
+            "tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing",
+        ]
     },
     "Sales Order": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.moq_validation_script.validate_moq"
+        "before_save": [
+            "tiered_pricing_moq.moq_validation_script.validate_moq",
+            "tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing",
+        ]
     },
     "Purchase Order": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.moq_validation_script.validate_moq"
+        "before_save": "tiered_pricing_moq.moq_validation_script.validate_moq",
     },
     "Blanket Order": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.moq_validation_script.validate_moq"
+        "before_save": "tiered_pricing_moq.moq_validation_script.validate_moq",
     },
     "Delivery Note": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.moq_validation_script.validate_moq"
+        "before_save": [
+            "tiered_pricing_moq.moq_validation_script.validate_moq",
+            "tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing",
+        ]
     },
     "Purchase Receipt": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.moq_validation_script.validate_moq"
-    }
-}
-#_____________________________________________________________________________________________________________________________________
-
-
-
-
-
-# tiered pricing calcutions for all sales processes
-doc_events = {
-    "Quotation": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing"
-    },
-    "Sales Order": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing"
+        "before_save": "tiered_pricing_moq.moq_validation_script.validate_moq",
     },
     "Sales Invoice": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing"
-    },
-    "Delivery Note": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing"
+        "before_save": "tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing",
     },
     "Sales Receipt": {
-        "before_save": "tiered_pricing_moq.tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing"
-    }
+        "before_save": "tiered_pricing_moq.tiered_pricing_calculation.apply_tiered_pricing",
+    },
 }
-
-
-
-
